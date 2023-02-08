75506d3967f7e99d900319e5db2830c27f2cdf4a
BinhBender <binhtnguyen123@gmail.com>
2023-02-08 08:25:37 -0800

    parse

--- TextParser.h
+++ TextParser.h
@@ -9,9 +9,23 @@ class TextParser{
 	//this will most likely take the longest
 	int wordCount;
 	std::istream ifile;
-	std::string 
+	std::string tokens;
 	TextParser();
 	//constructor for file
 	TextParser(string fileName);
+	
+	bool Parse(){
+		//check if the file is open or not before use
+		if(!ifile.is_open()) return false;
+		
+		//loops through the file for each word
+		while(ifile){
+			std::string dummy;
+			ifile >> dummy;
+			
+			if(dummy.size() == 1) 
+		}
+	
+	}
 	
 };
