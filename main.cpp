77e89aa89612ab62818abcbc6703e8d3c4e49498
BinhBender <binhtnguyen123@gmail.com>
2023-02-07 18:56:37 -0800

    Added a basic plan for program, started on timer class to measure speed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
 
 int main(){
 
 	std::cout << "Hello World"<< std::endl;
+	
+	//parse through a text file
+	//tokenize the sentences so that each word is its own string
+	//if any of characters like ' or `~ is adjacent to other chars, include them
+	//otherwise remove any strays of them.
+	
+	//when checking for the alphabetical order of the characters
+	//check the ascii of the characters
+	//lower = better
+	//if the chars are the same, go to the next char position
+	//if string A runs out of chars before string B, choose the shorter one
+	
+	//Quick--
+	//Quickly
+	
+	//make function for timing
+	
+	
+	//Insertion sort
+	//Selection Sort
+	//Merge Sort
+	//Quick Sort
+	
 }
 
---
+++ timer.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <chrono>
+
+class timer{
+
+	std::chrono::high_resolution_clock::time_point t = std::chrono::high_resolution_clock::now();
+	
+	
+	
+	
+}
